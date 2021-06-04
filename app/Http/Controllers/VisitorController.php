b924d6013b8e8c76a7c4b4dec5c0049afd92d118
K. M. Shawkat Zamil <43521098+KMSZamil@users.noreply.github.com>
2021-06-05 01:29:38 +0600

    Visitors Menu Added

--- app/Http/Controllers/VisitorController.php
+++ app/Http/Controllers/VisitorController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\VisitorsModel;
 
 class VisitorController extends Controller
 {
     function index(){
 
+        $VisitorsDate = json_decode(VisitorsModel::all());
+        return view('visitor',['VisitorsData'=>$VisitorsDate]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\VisitorController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,3 +16,4 @@ use App\Http\Controllers\HomeController;
 */
 
 Route::get('/', [HomeController::class, 'dashboard']);
+Route::get('/visitors', [VisitorController::class, 'index']);
