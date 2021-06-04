12bdbc9eab5308b5cb7fc4d01142c7cf85b0215f
K. M. Shawkat Zamil <43521098+KMSZamil@users.noreply.github.com>
2021-06-05 01:10:12 +0600

    Dashboard added

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,13 +7,13 @@ use App\Models\VisitorsModel;
 
 class HomeController extends Controller
 {
-    function index(){
+    function dashboard(){
 
         $UserIp = $_SERVER['REMOTE_ADDR'];
         date_default_timezone_set('Asia/Dhaka');
         $DateTime=date('Y-m-d h:i:sa');
         VisitorsModel::insert(['ip_address'=>$UserIp, 'visit_time'=>$DateTime]);
 
-        return view('home');
+        return view('dashboard');
     }
 }
---
+++ resources/views/visitor.blade.php
@@ -0,0 +1,8 @@
+@extends('index')
+
+@section('main_content')
+
+
+
+
+@endsection
