895b1135e62092b8b4d8b5c79f28834ff4783bb3
K. M. Shawkat Zamil <43521098+KMSZamil@users.noreply.github.com>
2021-06-05 09:22:49 +0600

    UserManager Controller Added

--- resources/views/inc/menubar.blade.php
+++ resources/views/inc/menubar.blade.php
@@ -87,13 +87,15 @@
                     </ul>
                 </li>
 
-                
+                <li class="menu menu-heading">
+                    <div class="heading"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-circle"><circle cx="12" cy="12" r="10"></circle></svg><span>Settings</span></div>
+                </li>
 
-                <li class="menu">
-                    <a href="map_jvector.html" aria-expanded="false" class="dropdown-toggle">
+                <li class="{{ request()->is('usermanager') ? 'active' : '' }}">
+                    <a href="{{url('/usermanager')}}" aria-expanded="false" class="dropdown-toggle">
                         <div class="">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-map"><polygon points="1 6 1 22 8 18 16 22 23 18 23 2 16 6 8 2 1 6"></polygon><line x1="8" y1="2" x2="8" y2="18"></line><line x1="16" y1="6" x2="16" y2="22"></line></svg>
-                            <span>Maps</span>
+                            <span>User Manager</span>
                         </div>
                     </a>
                 </li>
---
+++ resources/views/usermanager/view.blade.php
@@ -0,0 +1,42 @@
+@extends('index')
+
+@section('main_content')
+
+    <div class="row layout-top-spacing" id="cancel-row">
+        <div class="col-xl-12 col-lg-12 col-sm-12  layout-spacing">
+            <div class="widget-content widget-content-area br-6">
+                <div class="table-responsive mb-4 mt-4">
+                    <table id="zero-config" class="table table-hover" style="width:100%">
+                        <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>User ID</th>
+                            <th>User Name</th>
+                            <th>Password</th>
+                            <th>Designation</th>
+                            <th>Email</th>
+                            <th class="no-content"></th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach ($rows as $row)
+                            <tr>
+                                <td>{{ $r->id }}</td>
+                                <td>{{ $r->ip_address }}</td>
+                                <td>{{ $r->visit_time }}</td>
+                                <td>{{ $r->visit_time }}</td>
+                                <td>{{ $r->visit_time }}</td>
+                                <td>{{ $r->visit_time }}</td>
+                                <td></td>
+                            </tr>
+                        @endforeach
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+@endsection
