a3302ea01b5530f37e925dd77ec73a21520c031a
K. M. Shawkat Zamil <43521098+KMSZamil@users.noreply.github.com>
2021-06-05 19:16:07 +0600

    Update menubar.blade.php

--- resources/views/inc/menubar.blade.php
+++ resources/views/inc/menubar.blade.php
@@ -78,15 +78,15 @@
                         <li class="{{ request()->is('/') ? 'active' : '' }}">
                             <a href="{{url('/')}}"> Analytics </a>
                         </li>
-                        <li>
-                            <a href="#"> Sales </a>
-                        </li>
+{{--                        <li>--}}
+{{--                            <a href="#"> Sales </a>--}}
+{{--                        </li>--}}
                         <li class="{{ request()->is('visitors') ? 'active' : '' }}">
                             <a href="{{url('/visitors')}}"> Visitors </a>
                         </li>
                     </ul>
                 </li>
-                
+
                 <li class="menu menu-heading">
                     <div class="heading"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-circle"><circle cx="12" cy="12" r="10"></circle></svg><span>SETTINGS</span></div>
                 </li>
