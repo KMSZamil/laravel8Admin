2657852b7eb4253967a9441f044156d9be0f81e7
K. M. Shawkat Zamil <43521098+KMSZamil@users.noreply.github.com>
2021-06-05 18:52:16 +0600

    Doc Template Added

--- resources/views/inc/menubar.blade.php
+++ resources/views/inc/menubar.blade.php
@@ -86,19 +86,20 @@
                         </li>
                     </ul>
                 </li>
-
+                
                 <li class="menu menu-heading">
-                    <div class="heading"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-circle"><circle cx="12" cy="12" r="10"></circle></svg><span>Settings</span></div>
+                    <div class="heading"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-circle"><circle cx="12" cy="12" r="10"></circle></svg><span>SETTINGS</span></div>
                 </li>
 
-                <li class="{{ request()->is('usermanager') ? 'active' : '' }}">
+                <li class="menu {{ request()->is('usermanager') ? 'active' : '' }}">
                     <a href="{{url('/usermanager')}}" aria-expanded="false" class="dropdown-toggle">
                         <div class="">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-map"><polygon points="1 6 1 22 8 18 16 22 23 18 23 2 16 6 8 2 1 6"></polygon><line x1="8" y1="2" x2="8" y2="18"></line><line x1="16" y1="6" x2="16" y2="22"></line></svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-layout"><rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect><line x1="3" y1="9" x2="21" y2="9"></line><line x1="9" y1="21" x2="9" y2="9"></line></svg>
                             <span>User Manager</span>
                         </div>
                     </a>
                 </li>
+
             </ul>
         </nav>
 
