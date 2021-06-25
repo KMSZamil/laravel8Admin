37d536d0507605c12901d0de1f401d55a6545295
K. M. Shawkat Zamil <43521098+KMSZamil@users.noreply.github.com>
2021-06-25 14:58:57 +0600

    menifest and service worker update

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,3 +4,14 @@ workbox.routing.registerRoute(
     ({request}) => request.destination==='image',
     new workbox.strategies.NetworkFirst()
 );
+
+// const CACHE_NAME = "version-1";
+// const self = this;
+// self.addEventListener('install', (event) => {
+//     caches.open(CACHE_NAME).then((cache => {
+//         console.log('Cache opened');
+//     }))
+// });
+//
+// self.addEventListener('fetch',() => console.log("fetch"));
+//
